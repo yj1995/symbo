468db12f04b08bbb72d6c9912a1d00df4a756626
Yash Jain <yjain@cainc.com>
2019-12-16 19:51:38 +0530

    add page details

--- src/App/RoutePath.js
+++ src/App/RoutePath.js
@@ -5,20 +5,6 @@ import { PlanDetail } from './component/PlanDetail';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 class RoutePath extends Component {
-  constructor(props) {
-    super(props)
-    if (window.performance) {
-      if (performance.navigation.type == 1) {
-        let currentpath = window.location.pathname.split('/');
-        if (currentpath.length > 1) {
-          window.location.pathname = '/';
-        } else {
-          window.location.pathname = `/${currentpath[1]}/`;
-        }
-      }
-    }
-  }
-
   render() {
     console.log(window.location.pathname);
     return (
